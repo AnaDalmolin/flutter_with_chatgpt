377940571b209fc28a45fd544fbd2f21309d03f7
AnaDalmolin <55006937+AnaDalmolin@users.noreply.github.com>
2023-11-20 15:16:54 -0300

    Update app_config.dart

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   AppConfig._();
 
-  static String get getOpenAIAPIKey => "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";
+  static String get getOpenAIAPIKey => "Chave API";
 }
